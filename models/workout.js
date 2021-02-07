2ccd61fd12db8a2f0a54b5e441d29b15f49644f2
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-02-07 17:51:27 -0600

    Fixing and properly define excerices

--- models/workout.js
+++ models/workout.js
@@ -8,12 +8,32 @@ const workout = new Schema({
   },
   exercises: [
     {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
+      type: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+        required: true,
+      },
+      reps: {
+        type: Number,
+        required: true,
+      },
+      sets: {
+        type: Number,
+        required: true,
+      },
     },
   ],
 });
