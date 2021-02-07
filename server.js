f9441bb0fad10539b4f1cd67d983d0557d5cb316
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-02-07 17:10:31 -0600

    Adding code provided to connect to mongodb via heroku

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(express.json());
 
 app.use(express.static("pulic"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
