fa9e4640210e5ce2cdc21982c4a4ea13973863c0
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-02-07 17:33:10 -0600

    Defining day

--- models/workout.js
+++ models/workout.js
@@ -2,6 +2,9 @@ const mongoose = require("mongodb");
 const Schema = mongoose.Schema;
 
 const workout = new Schema({
-  day: {},
+  day: {
+    type: Date,
+    default: Date.now,
+  },
   exercises: {},
 });
