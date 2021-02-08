54aa2b109122722980d65b20c40c73a770c1b1e7
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-02-07 20:30:31 -0600

    Adding exercise routes

--- router/htmlroutes.js
+++ router/htmlroutes.js
@@ -10,4 +10,8 @@ module.exports = function (app) {
   app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
+
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
 };
