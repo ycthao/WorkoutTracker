143f89cca6cb355af960808bbe3cf6bdc6dafbf2
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-02-07 20:29:09 -0600

    Add stats routes

--- router/htmlroutes.js
+++ router/htmlroutes.js
@@ -6,4 +6,8 @@ module.exports = function (app) {
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
 };
