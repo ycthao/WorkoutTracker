4ef7206b0cec546abbe8c1345800b6027676a113
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-02-10 16:02:17 -0600

    Adding routes to server.js

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const mongojs = require("mongojs");
 
-const PORT = process.env.PORT || 3000;
+const databaseUrl = "workout";
+const collections = ["workouts"];
+const db = mongojs(databaseUrl, collections);
+
+const PORT = process.env.PORT || 8080;
 
 const Workout = require("./models");
 
@@ -16,15 +21,31 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
-require("./router/apiroutes.js")(app);
-require("./router/htmlroutes.js")(app);
+// require("./router/apiroutes.js")(app);
+// require("./router/htmlroutes.js")(app);
+
+module.exports = function (app) {
+  app.get("/", (req, res) => {
+    console.log("index");
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
+  app.get("/stats", (req, res) => {
+    console.log("status");
+    res.sendFile(path.join(__dirname, "public/stats.html"));
+  });
+
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/exercise.html"));
+  });
+};
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
