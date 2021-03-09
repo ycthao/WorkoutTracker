f651fd8c404a03c8827acf554670cd1081874c97
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 10:59:38 -0600

    adding range

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const Workout = require("../models/workout");
 
 module.exports = (app) => {
   // workout call -- getting all workouts
@@ -7,15 +8,35 @@ module.exports = (app) => {
       if (err) {
         console.log(err);
       } else {
-        console.log("working");
         res.json(workouts);
       }
     });
   });
 
   // create call -- creating workout
-  app.post("/api/workouts", (req, res) => {});
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+      .then((newWorkout) => {
+        console.log("created new exercise");
+        res.json(newWorkout);
+      })
+      .catch((err) => {
+        console.log("not working");
+        res.status(400).json(err);
+      });
+  });
 
   // update call -- updating workout
   app.put("/api/workouts/:id", (req, res) => {});
+
+  // get workout range
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then((workout) => {
+        res.json(workout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 };
