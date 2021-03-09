a8f592d459e3ce5af78a6ac86c1ebb9e0b0b42f3
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 09:22:02 -0600

    Adding code to connect to mongoose and for heroku deployment

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
