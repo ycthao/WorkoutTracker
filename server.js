87490d1abd50325c2a97fbf8cc5f05b3837063c5
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 10:09:31 -0600

    comment out dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-require("dotenv").config();
+//require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
