e76a5b54c9636b705c5f74402c22a973c2975aa9
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 10:19:32 -0600

    just making the initial call for post and put

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 
 module.exports = (app) => {
+  // workout call -- getting all workouts
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({}, (err, workouts) => {
       if (err) {
@@ -11,4 +12,10 @@ module.exports = (app) => {
       }
     });
   });
+
+  // create call -- creating workout
+  app.post("/api/workouts", (req, res) => {});
+
+  // update call -- updating workout
+  app.put("/api/workouts/:id", (req, res) => {});
 };
