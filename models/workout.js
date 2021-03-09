bf621202b02563da5f4a0e58dde07f6eb6c65451
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 10:11:22 -0600

    Changing name calls

--- models/workout.js
+++ models/workout.js
@@ -1,43 +1,50 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workout = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: true,
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: true,
-      },
-      duration: {
-        type: Number,
-        required: true,
-      },
-      weight: {
-        type: Number,
-        required: true,
-      },
-      reps: {
-        type: Number,
-        required: true,
-      },
-      sets: {
-        type: Number,
-        required: true,
-      },
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
-});
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        duration: {
+          type: Number,
+          required: true,
+        },
+        weight: {
+          type: Number,
+          required: true,
+        },
+        reps: {
+          type: Number,
+          required: true,
+        },
+        sets: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
-const Workout = mongoose.model("Workout", workout);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
