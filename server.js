77078b8eb55cc807638d33332f9b66c1853fbed7
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 09:19:45 -0600

    defining port and app uses

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
